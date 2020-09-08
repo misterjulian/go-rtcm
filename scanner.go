5d6066b6fcd11bc85ab117843afc1492f725b54d
Julian Vasiliev <julian.vasiliev@gmail.com>
2020-09-08 10:19:32 +0300

    Removed ctx & renamed to scanner

--- scanner.go
+++ scanner.go
@@ -2,45 +2,37 @@ package rtcm3
 
 import (
 	"bufio"
-	"context"
 	"io"
 
 	"github.com/misterjulian/go-rtcm/messages"
 )
 
 type (
-	Rtcm interface {
-		Next() (message messages.Message, err error)
-		NextFrame() (frame *Frame, err error)
+	Scanner interface {
+		Scan() (message messages.Message, err error)
+		ScanFrame() (frame *Frame, err error)
 	}
 
-	rtcm struct {
-		ctx    context.Context
+	scanner struct {
 		Reader *bufio.Reader
 	}
 )
 
 // New returns a Rtcm interface for getting next rtcm v3 message
-func New(ctx context.Context, r io.Reader) Rtcm {
-	return &rtcm{ctx: ctx, Reader: bufio.NewReader(r)}
+func NewScanner(r io.Reader) Scanner {
+	return &scanner{Reader: bufio.NewReader(r)}
 }
 
-func (s *rtcm) Next() (message messages.Message, err error) {
-	frame, err := s.NextFrame()
+func (s *scanner) Scan() (message messages.Message, err error) {
+	frame, err := s.ScanFrame()
 	if err != nil {
 		return nil, err
 	}
 	return messages.DeserializeMessage(frame.Payload), err // probably have DeserializeMessage return err
 }
 
-func (s *rtcm) NextFrame() (frame *Frame, err error) {
+func (s *scanner) ScanFrame() (frame *Frame, err error) {
 	for {
-		select {
-		case <-s.ctx.Done():
-			return frame, s.ctx.Err()
-		default:
-		}
-
 		frame, err := DeserializeFrame(s.Reader)
 		if err != nil {
 			if err.Error() == ErrInvalidPreamble || err.Error() == ErrInvalidCRC {
